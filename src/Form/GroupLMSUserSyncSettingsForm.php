80900e12db3969f33eea51e5f58a8d054d79659f
Daniel Rodriguez <danrod@gmail.com>
2024-04-10 15:16:38 -0400

    Implementation of the GroupLMSUserSyncSettingsForm class

--- src/Form/GroupLMSUserSyncSettingsForm.php
+++ src/Form/GroupLMSUserSyncSettingsForm.php
@@ -21,7 +21,7 @@ class GroupLMSUserSyncSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    return ['group.settings'];
+    return ['group_lms_user_sync.settings'];
   }
 
   /**
@@ -30,12 +30,12 @@ class GroupLMSUserSyncSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildForm($form, $form_state);
 
-    $config = $this->config('group.settings');
-    $form['use_admin_theme'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Use admin theme'),
-      '#description' => $this->t("Enables the administration theme for editing groups, members, etc."),
-      '#default_value' => $config->get('use_admin_theme'),
+    $config = $this->config('group_lms_user_sync.settings');
+
+    $form['api_endpoint_info'] = [
+      '#type' => 'key_select',
+      '#title' => $this->t('Secret key'),
+      '#default_value' => $config->get('api_endpoint_info'),
     ];
 
     return $form;
@@ -45,13 +45,13 @@ class GroupLMSUserSyncSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $config = $this->config('group.settings');
-    $conf_admin_theme = $config->get('use_admin_theme');
-    $form_admin_theme = $form_state->getValue('use_admin_theme');
+    $config = $this->config('group_lms_user_sync.settings');
+    $conf_api_endpoint_info = $config->get('api_endpoint_info');
+    $form_api_endpoint_info = $form_state->getValue('api_endpoint_info');
 
-    // Only rebuild the routes if the admin theme switch has changed.
-    if ($conf_admin_theme != $form_admin_theme) {
-      $config->set('use_admin_theme', $form_admin_theme)->save();
+    // Only rebuild the routes if the api_endpoint_info switch has changed.
+    if ($conf_api_endpoint_info != $form_api_endpoint_info) {
+      $config->set('api_endpoint_info', $form_api_endpoint_info)->save();
       \Drupal::service('router.builder')->setRebuildNeeded();
     }
 
