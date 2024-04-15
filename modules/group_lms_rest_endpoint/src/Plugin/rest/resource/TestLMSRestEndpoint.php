31fa45b71fa40a41c3bcf5dd6cf27788c42d71dc
Daniel Rodriguez <danrod@gmail.com>
2024-04-15 13:55:25 -0400

    Implementation of a custom test LMS Endpoint

---
+++ modules/group_lms_rest_endpoint/src/Plugin/rest/resource/TestLMSRestEndpoint.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\group_lms_rest_endpoint\Plugin\rest\resource;
+
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+
+/**
+ * Provides a Test LMS Rest Endpoint
+ *
+ * @RestResource(
+ *   id = "test_lms_rest_endpoint",
+ *   label = @Translation("Test LMS Rest Endpoint"),
+ *   uri_paths = {
+ *     "canonical" = "/group/test_lms_rest_endpoint"
+ *   }
+ * )
+ */
+class TestLMSRestEndpoint extends ResourceBase {
+
+  /**
+   * Responds to entity GET requests.
+   * @return \Drupal\rest\ResourceResponse
+   */
+  public function get() {
+    $response = ['message' => 'Hello, this is a rest service'];
+    return new ResourceResponse($response);
+  }
+}
