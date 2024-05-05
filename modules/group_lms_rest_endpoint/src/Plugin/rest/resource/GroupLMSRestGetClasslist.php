88d3d68c9d3178ba5e1eb949fd9a201f6d5a13c4
Daniel Rodriguez <danrod@gmail.com>
2024-05-05 19:09:42 -0400

    Refactoring of the group_lms_rest_endpoint submodule to implement the actual Group Class Endpoint API

--- modules/group_lms_rest_endpoint/src/Plugin/rest/resource/GroupLMSRestGetClasslist.php
+++ modules/group_lms_rest_endpoint/src/Plugin/rest/resource/GroupLMSRestGetClasslist.php
@@ -6,17 +6,17 @@ use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
 
 /**
- * Provides a Test LMS Rest Endpoint
+ * Provides a Class List Endpoint
  *
  * @RestResource(
- *   id = "test_lms_rest_endpoint",
- *   label = @Translation("Test LMS Rest Endpoint"),
+ *   id = "group_lms_rest_get_classlist",
+ *   label = @Translation("Group LMS Rest Get Class list"),
  *   uri_paths = {
  *     "canonical" = "/api/le/{version}/{orgUnitId}/classlist/paged"
  *   }
  * )
  */
-class TestLMSRestEndpoint extends ResourceBase {
+class GroupLMSRestGetClasslist extends ResourceBase {
 
   /**
    * Responds to entity GET requests.
