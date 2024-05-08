0f2f1fd7988d8eb48c484798f708725325cefe57
Daniel Rodriguez <danrod@gmail.com>
2024-05-08 19:46:33 -0400

    Refactoring of the drush command to pull users/groups from the LMI endpoint

--- src/Drush/GroupLMSUserSyncCommands.php
+++ src/Drush/GroupLMSUserSyncCommands.php
@@ -2,21 +2,26 @@
 
 namespace Drupal\group_lms_user_sync\Drush;
 
+use Drush\Commands\DrushCommands;
+
 /**
- * Class DrushCommands.
+ * Class GroupLMSUserSyncCommands.
  *
  * @package Drupal\group_lms_user_sync\Drush
  */
-class DrushCommands {
+class GroupLMSUserSyncCommands extends DrushCommands {
 
   /**
    * Sync users/class groups from the LMI endpoint.
    *
    * @command gl:user-sync
+   * 
    * @aliases gl-us
+   * 
    * @usage gl-us
    */
   public function syncUsersGroups() {
+    $this->io()->success('Synced users/group from the LMI Endpoint !');
   }
 
 }
