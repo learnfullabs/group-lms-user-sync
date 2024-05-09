52cd24adac32ae06a2a68700e02d71343a7432da
Daniel Rodriguez <danrod@gmail.com>
2024-05-09 12:36:39 -0400

    Implementation of the Drush command to get the Rest API json data

--- src/Drush/GroupLMSUserSyncCommands.php
+++ src/Drush/GroupLMSUserSyncCommands.php
@@ -21,7 +21,14 @@ class GroupLMSUserSyncCommands extends DrushCommands {
    * @usage gl-us
    */
   public function syncUsersGroups() {
-    $this->io()->success('Synced users/group from the LMI Endpoint !');
+    $endpoint_id = \Drupal::config('group_lms_user_sync.settings')->get('api_endpoint_info') ?? "";
+
+    if (isset($endpoint_id) && !empty($endpoint_id)) {
+      // Use the keys API to get the Endpoint URL
+      $endpoint_url = \Drupal::service('key.repository')->getKey($endpoint_id)->getKeyValue();
+    }
+
+    $this->io()->success('Synced users/group from the LMI Endpoint !' . $endpoint_url);
   }
 
 }
