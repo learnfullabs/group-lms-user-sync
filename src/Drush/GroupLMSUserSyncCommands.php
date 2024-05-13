e050e88f00387cd167dd496ad6271a7d993d47ee
Daniel Rodriguez <danrod@gmail.com>
2024-05-13 13:42:15 -0400

    Initial implementation of GroupLMSUserSyncCommands

--- src/Drush/GroupLMSUserSyncCommands.php
+++ src/Drush/GroupLMSUserSyncCommands.php
@@ -3,6 +3,7 @@
 namespace Drupal\group_lms_user_sync\Drush;
 
 use Drush\Commands\DrushCommands;
+use Drush\group_lms_user_sync\GroupLMSUserSyncAPI;
 
 /**
  * Class GroupLMSUserSyncCommands.
@@ -21,50 +22,9 @@ class GroupLMSUserSyncCommands extends DrushCommands {
    * @usage gl-us
    */
   public function syncUsersGroups() {
-    $endpoint_id = \Drupal::config('group_lms_user_sync.settings')->get('api_endpoint_info') ?? "";
-    $api_version = "v1";
+    $drushHandler = new GroupLMSUSerSyncAPI();
 
-    if (isset($endpoint_id) && !empty($endpoint_id)) {
-      // Use the keys API to get the Endpoint URL
-      $endpoint_url = \Drupal::service('key.repository')->getKey($endpoint_id)->getKeyValue();
-    
-      if (isset($endpoint_url) && !empty($endpoint_url)) {
-        // Create an httpClient Object that will be used for all the requests.
-        $client = \Drupal::httpClient();
-
-        // Pulling the data from the API
-        $group_ids = [1, 2, 3];
-
-        foreach ($group_ids as $group_id) {
-          try {
-            $request = $client->get($endpoint_url . '/' . $api_version . '/' . $group_id . '/classlist/paged', [
-              'http_errors' => TRUE,
-              'query' => [
-                '_format' => 'json'
-              ]
-            ]);
-          
-            if (!empty($request)) {
-              $this->io()->success('Got data from the Endpoint !' . $request->getBody());
-              $classroom = json_decode($request->getBody());
-
-              foreach($classroom as $student) {
-                $this->io()->success('Student Identifier: ' . $student->Identifier);
-                /* First, check if the user (identified by Email or Username) exists, if not, create the user */
-                /* If it exists, enroll the user into the course identified by OrgDefinedId (OU field from the Group field) */
-                /* Check for the RoleID field, should map to the Drupal User Role */
-              }
-            }
-          } catch (\Exception $e) {
-            watchdog_exception('group_lms_user_sync', $e);
-          }
-        }
-      } else {
-        $this->io()->error('Endpoint URL was not set');
-      }
-    }
-
-    $this->io()->success('Synced users/group from the LMI Endpoint !' . $endpoint_url);
+    $drushHandler->syncUsersToGroups();
   }
 
 }
---
+++ src/GroupLMSUserSyncAPI.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\group_lms_user_sync;
+
+/**
+ * GroupLMSUserSyncAPI.
+ *
+ * Class that provides methods to parse/process the data (JSON) coming from the API
+ */
+class GroupLMSUserSyncAPI {
+
+  /**
+   * Constructor for the GroupLMSUserSyncAPI objects.
+   */
+  public function __construct() {
+  }
+
+  /**
+   * Sync users/class groups from the LMI AP Endpoint to the Drupal Groups.
+   */
+  private function syncUsersToGroups() {
+    $endpoint_id = \Drupal::config('group_lms_user_sync.settings')->get('api_endpoint_info') ?? "";
+    $api_version = "v1";
+
+    if (isset($endpoint_id) && !empty($endpoint_id)) {
+      // Use the keys API to get the Endpoint URL
+      $endpoint_url = \Drupal::service('key.repository')->getKey($endpoint_id)->getKeyValue();
+    
+      if (isset($endpoint_url) && !empty($endpoint_url)) {
+        // Create an httpClient Object that will be used for all the requests.
+        $client = \Drupal::httpClient();
+
+        // Pulling the data from the API
+        $group_ids = [1, 2, 3];
+
+        foreach ($group_ids as $group_id) {
+          try {
+            $request = $client->get($endpoint_url . '/' . $api_version . '/' . $group_id . '/classlist/paged', [
+              'http_errors' => TRUE,
+              'query' => [
+                '_format' => 'json'
+              ]
+            ]);
+          
+            if (!empty($request)) {
+              $this->io()->success('Got data from the Endpoint !' . $request->getBody());
+              $classroom = json_decode($request->getBody());
+
+              foreach($classroom as $student) {
+                $this->io()->success('Student Identifier: ' . $student->Identifier);
+                /* First, check if the user (identified by Email or Username) exists, if not, create the user */
+                /* If it exists, enroll the user into the course identified by OrgDefinedId (OU field from the Group field) */
+                /* Check for the RoleID field, should map to the Drupal User Role */
+              }
+            }
+          } catch (\Exception $e) {
+            watchdog_exception('group_lms_user_sync', $e);
+          }
+        }
+      } else {
+        $this->io()->error('Endpoint URL was not set');
+      }
+    }
+
+    $this->io()->success('Synced users/group from the LMI Endpoint !' . $endpoint_url);
+  }
+
+}
