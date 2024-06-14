692604c807977ef45682d29a99ac4efa8f00af64
Daniel Rodriguez <danrod@gmail.com>
2024-06-13 21:41:21 -0400

    Issue # 9: Added views file in the config directory

--- src/GroupLMSUserSyncAPI.php
+++ src/GroupLMSUserSyncAPI.php
@@ -192,6 +192,7 @@ class GroupLMSUserSyncAPI implements ContainerInjectionInterface {
                           $count_updated_groups[$user_id_api] = $group->id();
                           $group_name = $group->label();
 
+                          // Logs Group Activity
                           $group_log_event = GroupLog::create(array(
                             'name' => $group_name . "-" . $group_id_api . "-" . $user_obj->getAccountName(),
                             'group_name' => $group->label(),
@@ -240,6 +241,16 @@ class GroupLMSUserSyncAPI implements ContainerInjectionInterface {
     
                             $count_updated_groups[$user_id_api] = $group->id();
                             $group_name = $group->label();
+
+                            // Logs Group Activity
+                            $group_log_event = GroupLog::create(array(
+                              'name' => $group_name . "-" . $group_id_api . "-" . $user_new->getAccountName(),
+                              'group_name' => $group->label(),
+                              'group_ou' => $group_id_api,
+                              'username' => $user_new->getAccountName(),
+                              'enroll_status' => 1,
+                            ));
+                            $group_log_event->save();
 
                             if ($print_debug_messenger_info) {
                               $this->messenger->addStatus(t("Added user @username to group @groupname", ['@username' => $username_api, '@groupname' => $group_id_api]));
@@ -342,6 +353,16 @@ class GroupLMSUserSyncAPI implements ContainerInjectionInterface {
             $count_updated_groups[$user_id_api] = $group->id();
             $group_name = $group->label();
 
+            // Logs Group Activity
+            $group_log_event = GroupLog::create(array(
+              'name' => $group_name . "-" . $group_id_api . "-" . $user_obj->getAccountName(),
+              'group_name' => $group->label(),
+              'group_ou' => $group_id_api,
+              'username' => $user_obj->getAccountName(),
+              'enroll_status' => 1,
+            ));
+            $group_log_event->save();
+
             $this->messenger->addStatus(t("Added user @username to group @groupname", ['@username' => $username_api, '@groupname' => $group_id_api]));
             $this->logger->notice("Added user @username to group @groupname", ['@username' => $username_api, '@groupname' => $group_id_api]);
           }
@@ -376,6 +397,16 @@ class GroupLMSUserSyncAPI implements ContainerInjectionInterface {
 
               $count_updated_groups[$user_id_api] = $group->id();
               $group_name = $group->label();
+
+              // Logs Group Activity
+              $group_log_event = GroupLog::create(array(
+                'name' => $group_name . "-" . $group_id_api . "-" . $user_new->getAccountName(),
+                'group_name' => $group->label(),
+                'group_ou' => $group_id_api,
+                'username' => $user_new->getAccountName(),
+                'enroll_status' => 1,
+              ));
+              $group_log_event->save();
 
               $this->messenger->addStatus(t("Added user @username to group @groupname", ['@username' => $username_api, '@groupname' => $group_id_api]));
               $this->logger->notice("Added user @username to group @groupname", ['@username' => $username_api, '@groupname' => $group_id_api]);
